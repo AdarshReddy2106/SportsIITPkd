0c8c8d755b74f510c544b5aa054f3df474dbec00
Adarsh <adarshreddy2106@gmail.com>
2025-05-31 16:53:49 +0530

    Added Services Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { Layout } from './components/shared/Layout';
 import { Hero } from './components/sections/Hero';
+import { Brands } from './components/sections/Brands';
+import { Services } from './components/sections/Services';
 
 function App() {
   return <Layout title="Sports IITPkd" >
       <Hero />
+      <Brands />
+      <Services />
   </Layout>  
 }
 
